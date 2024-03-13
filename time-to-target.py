984361ea434288447154e91fd0cd01a2b140c868
bean crab-apple <deano.capellari@gmail.com>
2024-03-13 19:20:43 +0000

    IT WORKS

--- time-to-target.py
+++ time-to-target.py
@@ -14,38 +14,21 @@ def flightsim(speed,weight,cd,target):
 def findcd(speedbow,speedtarget,weight,target):
     cdmatch = False
     testcd = 0
+    mode = 1
+    step = 0.001
     while cdmatch == False:
         cdtestfinalparams = flightsim(speedbow,weight,testcd,target)
         print(cdtestfinalparams[0])
-        if (cdtestfinalparams[0] - speedtarget) < 0:
-            cdmatch = True
-        else:
-            testcd+=0.001
-    cdmatch = False
-    while cdmatch == False:
-        cdtestfinalparams = flightsim(speedbow,weight,testcd,target)
-        print(cdtestfinalparams[0])
-        if (cdtestfinalparams[0] - speedtarget) > 0:
-            cdmatch = True
-        else:
-            testcd-=0.00001
-    cdmatch = False
-    while cdmatch == False:
-        cdtestfinalparams = flightsim(speedbow,weight,testcd,target)
-        print(cdtestfinalparams[0])
-        if (cdtestfinalparams[0] - speedtarget) < 0:
-            cdmatch = True
-        else:
-            testcd+=0.0000001
-    cdmatch = False
-    while cdmatch == False:
-        cdtestfinalparams = flightsim(speedbow,weight,testcd,target)
-        print(cdtestfinalparams[0])
-        if (cdtestfinalparams[0] - speedtarget) > 0:
+        if -0.000000001 < (cdtestfinalparams[0] - speedtarget) < 0.000000001:
             cdmatch = True
             return testcd
         else:
-            testcd-=0.000000001
+            if mode*(cdtestfinalparams[0] - speedtarget) < 0:
+                mode = (-1)*mode
+                step = (-0.1)*step
+                testcd = testcd+step
+            else:
+                testcd = testcd+step
 
 speedbow = 0.3048000000000021701760000000154516531200001100157702144007833122* int(input("what is your first arrow's speed out of the bow in fps?"))
 speedtarget = 0.3048000000000021701760000000154516531200001100157702144007833122* int(input("what is your first arrow's speed at the target in fps?"))
@@ -59,8 +42,8 @@ speed2 = math.sqrt(ke/(0.5*weight2))
 
 cd = findcd(speedbow,speedtarget,weight1,target)
 
-timefinal1 = flightsim(speedbow,weight1,cd,target)
-timefinal2 = flightsim(speed2,weight2,cd,target)
+finalparams1 = flightsim(speedbow,weight1,cd,target)
+finalparams2 = flightsim(speed2,weight2,cd,target)
 
-print("arrow 1: " + str(timefinal1[1]) + "s to target")
-print("arrow 2: " + str(timefinal2[1]) + "s to target")
+print("arrow 1: " + str(finalparams1[1]) + "s to target")
+print("arrow 2: " + str(finalparams2[1]) + "s to target")
